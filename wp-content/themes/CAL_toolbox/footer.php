03740fd1d3beb67cde96a1fa721edfb2770b4a4e
Stan Sabatier <stan@sapienssapide.fr>
2012-01-04 18:57:50 +0100

    mise en place des contenus dans le footer

--- wp-content/themes/CAL_toolbox/footer.php
+++ wp-content/themes/CAL_toolbox/footer.php
@@ -13,6 +13,25 @@
 </div><!-- #main -->
 
 <footer id="colophon" role="contentinfo">
+    <div id="footer-adresses">
+        <h5>CONTACT</h5>
+        Vous pouvez contacter nos bureaux à <a href="/le-cabinet/nos-adresses/bureau-de-paris/">Paris</a>,
+        <a href="/le-cabinet/nos-adresses/bureau-de-poitiers/">Poitiers</a> et
+        <a href="/le-cabinet/nos-adresses/bureau-de-seville/">Séville</a>.
+    </div>
+    <div id="footer-filActu">
+        <h5>FIL D'ACTUALITÉS</h5>
+        Pour recevoir nos actualités, au fil des publications, abonnez-vous à notre <a href="/rss/">fil RSS</a>.
+    </div>
+    <div id="footer-lettreInfo">
+        <h5>LETTRE D'INFORMATION</h5>
+        Vous pouvez vous abonner à notre <a href="/?post_type=lettreinformation">lettre d'information mensuelle</a>.
+    </div>
+    <div id="footer-liensUtiles">
+        <h5>LIENS UTILES</h5>
+        Nous vous conseillons des liens vers des
+        <a href="/liens-utiles/">sites</a> pour vous informer en droit des affaires.
+    </div>
     <div id="site-generator">
         <div class="mentions">
             <a href="/mentions-legales/">Mentions légagles</a> &copy; 2011 - 2012 Cabinet d'Avocats Leloup

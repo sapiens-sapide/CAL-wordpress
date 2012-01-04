c2d02941f7b8c562575d2c201fa29e74e4743a81
Stan Sabatier <stan@sapienssapide.fr>
2012-01-04 09:29:24 +0100

    modification de la page bookmarks pour qu'elle affiche une liste des "links" entrés via l'interface d'admin

--- wp-content/themes/CAL_toolbox/CAL_bookmarks.php
+++ wp-content/themes/CAL_toolbox/CAL_bookmarks.php
@@ -12,29 +12,8 @@ CAL_BOOKMARKS.PHP
 <div id="primary">
     <div id="content" role="main">
 
-        <?php
+        <?php wp_list_bookmarks('title_li=&category_before=&category_after='); ?>
 
-        $cat = get_cat_ID($category);
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        $post_per_page = 4; // -1 shows all posts
-        $do_not_show_stickies = 1; // 0 to show stickies
-        $args = array('post_type'      => 'publications',
-                      'posts_per_page' => 5);
-        $temp = $wp_query;  // assign orginal query to temp variable for later use
-        $wp_query = null;
-        $wp_query = new WP_Query($args);
-        if (have_posts()) :
-            while ($wp_query->have_posts()) : $wp_query->the_post();  ?>
-
-                <?php get_template_part('content', 'page'); ?>
-
-                <?php comments_template('', true); ?>
-
-                <?php endwhile; // end of the loop. ?>
-            <?php endif;
-
-        $wp_query = $temp;  //reset back to original query
-        ?>
     </div>
     <!-- #content -->
 </div><!-- #primary -->
